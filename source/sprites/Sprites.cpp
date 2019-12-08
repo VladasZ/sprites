f41cce0c290ce79123f35480e3b26ef7943194d5
Vladas <146100@gmail.com>
2019-12-08 15:00:01 +0300

    Log refactoring

--- source/sprites/Sprites.cpp
+++ source/sprites/Sprites.cpp
@@ -18,18 +18,21 @@ static SpriteDrawer* _drawer = nullptr;
 
 void config::set_drawer(SpriteDrawer* drawer) {
 #ifdef DEBUG
-	if (_drawer != nullptr)
-		Fatal("Sprite drawer must be set only once");
-	if (drawer == nullptr)
-		Fatal("Sprite drawer must be nonnull");
+	if (_drawer != nullptr) {
+        Fatal("Sprite drawer must be set only once");
+    }
+	if (drawer == nullptr) {
+        Fatal("Sprite drawer must be nonnull");
+    }
 #endif
 	_drawer = drawer;
 }
 
 SpriteDrawer* config::drawer() {
 #ifdef DEBUG
-	if (_drawer == nullptr)
-		Fatal("Sprite drawer is not set before first usage");
+	if (_drawer == nullptr) {
+        Fatal("Sprite drawer is not set before first usage");
+    }
 #endif
 	return _drawer;
 }
