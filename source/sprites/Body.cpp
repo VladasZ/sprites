d16217b6f3e8593f1ca79a53785f87ddaf64168d
Vladas Zakrevskis <146100@gmail.com>
2020-12-07 18:40:15 +0300

    removed platform, added body

---
+++ source/sprites/Body.cpp
@@ -0,0 +1,11 @@
+//
+//  Body.cpp
+//  sprites
+//
+//  Created by Vladas Zakrevskis on 07/12/20.
+//  Copyright © 2020 VladasZ. All rights reserved.
+//
+
+#include "Body.hpp"
+
+using namespace sprite;
---
+++ source/sprites/Body.hpp
@@ -0,0 +1,24 @@
+//
+//  Body.hpp
+//  sprites
+//
+//  Created by Vladas Zakrevskis on 07/12/20.
+//  Copyright © 2020 VladasZ. All rights reserved.
+//
+
+#pragma once
+
+#include "Sprite.hpp"
+
+
+namespace sprite {
+
+	class Body : public Sprite {
+
+	public:
+
+		using Sprite::Sprite;
+
+	};
+
+}
